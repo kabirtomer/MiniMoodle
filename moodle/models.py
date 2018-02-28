b2c1eff8180606af383f29533273b026a96c0e81
Kabir <kabirtomer@gmail.com>
2018-02-28 20:51:12 +0530

    Models update

--- moodle/models.py
+++ moodle/models.py
@@ -1,19 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils import timezone
 
 COURSE_NAME_LENGTH=10
 COURSE_SUBTITLE_LENGTH=100
+MESSAGE_TITLE_LENGTH=100
 # Create your models here.
 
-class Course(models.Model):
-	prof = models.ForeignKey('auth.User',on_delete=models.CASCADE, related_name="prof")
-	name = models.CharField(max_length=COURSE_NAME_LENGTH)
-	subtitle = models.CharField(max_length=COURSE_SUBTITLE_LENGTH, blank=True,  null=True)
-	start_date=models.DateTimeField(blank=True, null=True)
-	message= models.TextField(blank=True, null=True)
-	students = models.ManyToManyField('auth.User', related_name='students')
 
-	def __str__(self):
-		return self.name
+class Course(models.Model):
+    prof = models.ForeignKey('auth.User',on_delete=models.CASCADE, related_name="courses_teaching")
+    name = models.CharField(max_length=COURSE_NAME_LENGTH)
+    subtitle = models.CharField(max_length=COURSE_SUBTITLE_LENGTH, blank=True,  null=True)
+    start_date=models.DateTimeField(blank=True, null=True)
+    #registrations = models.ManyToManyField(Registration, related_name='course')
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+
+    CHOICES=(
+        (0, 'Student'),
+        (1, 'Professor')
+    )
+
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    is_prof = models.PositiveSmallIntegerField(choices=CHOICES,default=0)
+
+    def __str__(self):
+        return self.user.username + " profile"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+class Registration(models.Model):
+    course=models.ForeignKey(Course, on_delete=models.CASCADE, related_name="registrations")
+    student=models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name="registrations")
+    date=models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.student.username + " reg in " +self.course.name
+
+
+class Message(models.Model):
+    title=models.CharField(max_length=MESSAGE_TITLE_LENGTH)
+    text=models.TextField()
+    course=models.ForeignKey(Course, on_delete=models.CASCADE, related_name="messages")
+    date=models.DateTimeField(default=timezone.now)
 
 
 
--- moodle/views.py
+++ moodle/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
 
 # Create your views here.
+def homepage(request):
+	courses = models.Course.objects.all()
+	return render(request, 'homepage.html',{'courses':courses})
+
+def course_view(request,pk):
+
+	course = get_object_or_404(models.Course,pk=pk)
+	regs=course.registrations.all()
+	return render(request, 'course.html',{'course':course, 'regs':regs})
